0e40a85f35b93e799661b9388b73c49a908930aa
An Unsocial Pigeon <66815528+AnUnsocialPigeon@users.noreply.github.com>
2022-04-04 00:27:36 +0100

    Add project files.

---
+++ Image Overlayer/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Image_Overlayer {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            BGImage.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + @"\Background.png"));
+            BGImage.Opacity = TransparencySlider.Value / 10f;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        //Attach this to the MouseDown event of your drag control to move the window in place of the title bar
+        private void WindowDrag(object sender, MouseButtonEventArgs e) // MouseDown
+        {
+            ReleaseCapture();
+            SendMessage(new WindowInteropHelper(this).Handle,
+                0xA1, (IntPtr)0x2, (IntPtr)0);
+        }
+
+        //Attach this to the PreviewMousLeftButtonDown event of the grip control in the lower right corner of the form to resize the window
+        private void WindowResize(object sender, MouseButtonEventArgs e) //PreviewMousLeftButtonDown
+        {
+            HwndSource hwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource;
+            SendMessage(hwndSource.Handle, 0x112, (IntPtr)61448, IntPtr.Zero);
+        }
+        private void CloseBTN_Click(object sender, RoutedEventArgs e) {
+            Environment.Exit(0);
+            Close();
+        }
+
+        private void TransparencySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            BGImage.Opacity = TransparencySlider.Value / 10f;
+        }
+    }
+}
